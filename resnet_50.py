6ad78469df96bceb822f16166e217a063036121d
Xiaoyan Li <xiaoyanli629@gmail.com>
2020-03-31 22:10:16 -0400

    Initial commit

---
+++ resnet_50.py
@@ -0,0 +1,87 @@
+import torch
+import matplotlib.pyplot as plt
+
+model = torch.hub.load('pytorch/vision:v0.5.0', 'resnet18', pretrained=True)
+# or any of these variants
+# model = torch.hub.load('pytorch/vision:v0.5.0', 'resnet34', pretrained=True)
+# model = torch.hub.load('pytorch/vision:v0.5.0', 'resnet50', pretrained=True)
+# model = torch.hub.load('pytorch/vision:v0.5.0', 'resnet101', pretrained=True)
+# model = torch.hub.load('pytorch/vision:v0.5.0', 'resnet152', pretrained=True)
+model.eval()
+
+# Download an example image from the pytorch website
+import urllib
+url, filename = ("https://github.com/pytorch/hub/raw/master/dog.jpg", "dog.jpg")
+try: urllib.URLopener().retrieve(url, filename)
+except: urllib.request.urlretrieve(url, filename)
+
+# sample execution (requires torchvision)
+from PIL import Image
+from matplotlib import image
+from torchvision import transforms
+import numpy as np
+from matplotlib import cm
+from numpy import asarray
+
+input_image = Image.open(filename)
+
+preprocess = transforms.Compose([
+    transforms.Resize(256),
+    transforms.CenterCrop(224),
+    transforms.ToTensor(),
+    # transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+])
+input_tensor = preprocess(input_image)
+
+input_batch = input_tensor.unsqueeze(0) # create a mini-batch as expected by the model
+
+
+# move the input and model to GPU for speed if available
+if torch.cuda.is_available():
+    input_batch = input_batch.to('cuda')
+    model.to('cuda')
+
+with torch.no_grad():
+    output = model(input_batch)
+# Tensor of shape 1000, with confidence scores over Imagenet's 1000 classes
+# print("output:", output[0])
+# The output has unnormalized scores. To get probabilities, you can run a softmax on it.
+# print(torch.nn.functional.softmax(output[0], dim=0))
+
+# print("Model structure:", model)
+
+data = asarray(input_image)
+print(type(data))
+# summarize shape
+print(data.shape)
+
+image2 = Image.fromarray(data)
+
+# image2.show()
+print(data.dtype)
+
+
+image = image.imread(filename)
+# summarize shape of the pixel array
+print(image.dtype)
+print(image.shape)
+# display the array of pixels as an image
+# plt.imshow(image)
+# plt.show()
+
+# print(data[:, :, 0])
+# print(image[:, :, 0])
+
+# plt.imshow(data)
+# plt.show()
+
+print(input_tensor.numpy().shape)
+test =  np.zeros((224, 224, 3))
+test[:, :, 0] = input_tensor.numpy()[0, :, :]
+test[:, :, 1] = input_tensor.numpy()[1, :, :]
+test[:, :, 2] = input_tensor.numpy()[2, :, :]
+
+
+print(test.shape)
+plt.imshow(test)
+plt.show()
